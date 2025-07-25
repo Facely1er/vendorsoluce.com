79c06a2ed1c0a7c19571bde9cffe57fab03b0bf2
Facely1er <facelykande@gmail.com>
2025-07-25 08:29:25 -0400

    Updated SBOMAnalyzer.tsx

--- src/pages/SBOMAnalyzer.tsx
+++ src/pages/SBOMAnalyzer.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Card, CardHeader, CardTitle, CardContent } from '../ui/Card';
-import { Button } from '../ui/Button';
+import { Card, CardHeader, CardTitle, CardContent } from '../components/ui/Card';
+import { Button } from '../components/ui/Button';
 import { 
   Shield, 
   Building2,
