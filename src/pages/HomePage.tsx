f35a6869cbf23dbf24c4e314a01d89fcbc549786
Facely1er <facelykande@gmail.com>
2025-07-25 06:32:03 -0400

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -16,7 +16,7 @@ const HomePage: React.FC = () => {
       <AssessmentSection />
       <ValuePropositionSection />
       <FeatureSection />
-      <QuickToolsSection />
+      {/*  <QuickToolsSection /> */}
       <CTASection />
     </main>
   );
