b0a7ae7ae9e17a318d3ec3926ab7dca7a9f101fd
Facely1er <facelykande@gmail.com>
2025-07-26 11:08:02 -0400

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import HeroSection from '../components/home/HeroSection';
 import ValuePropositionSection from '../components/home/ValuePropositionSection';
-import AssessmentSection from '../components/home/AssessmentSection';
+{/*import AssessmentSection from '../components/home/AssessmentSection';*/}
 import FeatureSection from '../components/home/FeatureSection';
 import QuickToolsSection from '../components/home/QuickToolsSection';
 import CTASection from '../components/home/CTASection';
@@ -13,7 +13,7 @@ const HomePage: React.FC = () => {
   return (
     <main className="min-h-screen">
       <HeroSection />
-      <AssessmentSection />
+      {/*<AssessmentSection /> */}
       <ValuePropositionSection />
       <FeatureSection />
       {/*  <QuickToolsSection /> */}
