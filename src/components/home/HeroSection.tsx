bf5df326a41a2b2b435e1dc5b4f7cb35af16fb47
Facely1er <facelykande@gmail.com>
2025-07-26 22:15:09 -0400

    Updated HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -20,7 +20,7 @@ const HeroSection: React.FC = () => {
         style={{
           backgroundImage: 'url("/background_hero_section.png")',
           backgroundSize: 'cover',
-          backgroundPosition: '80% 70%',
+          backgroundPosition: '70% 70%',
           backgroundRepeat: 'no-repeat',
           
         }}
