3cf79bbce499aa1f2d7bc7d21aec14b53e77a5a3
Facely1er <facelykande@gmail.com>
2025-07-28 09:47:37 -0400

    Updated HeroSection.tsx

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -52,7 +52,7 @@ const HeroSection: React.FC = () => {
             <div className="flex justify-center mb-6">
             </div>
             
-            <h1 className="text-4xl md:text-4xl font-bold mb-6 opacity-0 animate-fade-in-up animate-delay-100">
+            <h1 className="text-4xl md:text-3xl font-bold mb-6 opacity-0 animate-fade-in-up animate-delay-100">
               {t('home.hero.title_line1')} <br />
               {t('home.hero.title_line2')}
             </h1>
